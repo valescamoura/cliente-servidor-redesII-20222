d067b75bb4d766bdc85b89afd9b6bc6c775b66a2
Victor Fernandes <victor.faria@olxbr.com>
2022-11-08 09:59:03 -0300

    atualizando com código de exemplo de client_usecase

--- servidor-registro/main.py
+++ servidor-registro/main.py
@@ -1,6 +1,14 @@
-import socket 
+import socket
+
+from server import Server 
+
+def client_usecase(data):
+    print(data)
+    pass
 
 def main():
+    s = Server()
+    s.run(client_usecase)
     pass
 
 if __name__ == '__main__':
