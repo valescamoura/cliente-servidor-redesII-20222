fafbbd0b49f0bdb3c95173bfa98d8418b1275859
Victor Fernandes <victor.faria@olxbr.com>
2022-11-08 09:49:40 -0300

    estruturando parte do servidor em classe

--- servidor-registro/server.py
+++ servidor-registro/server.py
@@ -2,38 +2,41 @@ import socket
 import os
 from _thread import *
 
-ServerSideSocket = socket.socket()
-host = '127.0.0.1'
-port = 2004
-ThreadCount = 0
+class Server:
+    def __init__(self, host='127.0.0.1', port=2004):
+        self.server_side_socket = socket.socket()
+        self.host = host
+        self.port = port
+        self.threadCount = 0
+        self.init_server()
 
-#init server
-try:
-    ServerSideSocket.bind((host, port))
-except socket.error as e:
-    print(str(e))
+    def init_server(self):
+        try:
+            self.server_side_socket.bind((self.host, self.port))
+        except socket.error as e:
+            print(str(e))
 
+        print('Socket is listening..')
+        self.server_side_socket.listen(5)
 
-print('Socket is listening..')
-ServerSideSocket.listen(5)
+    def run(self):
+        while True:
+            Client, address = self.server_side_socket.accept()
+            print('Connected to: ' + address[0] + ':' + str(address[1]))
+            start_new_thread(self.client_message_handler, (Client, ))
+            ThreadCount += 1
+            print('Thread Number: ' + str(ThreadCount))
 
+    def client_message_handler(self,connection):
+        connection.send(str.encode('Server is working:'))
+        while True:
+            data = connection.recv(2048)
+            response = 'Server message: ' + data.decode('utf-8')
+            print("Received server data: " + data.decode('utf-8'))
+            if not data:
+                break
+            connection.sendall(str.encode(response))
+        connection.close()
 
-def multi_threaded_client(connection):
-    connection.send(str.encode('Server is working:'))
-    while True:
-        data = connection.recv(2048)
-        response = 'Server message: ' + data.decode('utf-8')
-        print("Received server data: " + data.decode('utf-8'))
-        if not data:
-            break
-        connection.sendall(str.encode(response))
-    connection.close()
-
-while True:
-    Client, address = ServerSideSocket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-    #start_new_thread(multi_threaded_client, (Client, ))
-    multi_threaded_client(Client)
-    ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
-ServerSideSocket.close()
+    def close(self):
+        self.server_side_socket.close()
